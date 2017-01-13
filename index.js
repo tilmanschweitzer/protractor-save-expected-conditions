a35cf8c3ee5995f55487bd0103e0414a4df9dee7
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-01-13 13:28:03 +0100

    Rename module and export functions

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+	falseOnRaceConditionError: require('./src/falseOnRaceConditionError'),
+	saveVisibilityOf: require('./src/saveVisibilityOf'),
+	saveTextToBePresentInElement: require('./src/saveTextToBePresentInElement'),
+};
